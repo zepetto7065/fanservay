eaf4f4c7c1fddbf61c5b117819349964571928bf
유상문 <zepetto.yoo@gmail.com>
2022-01-09 16:50:59 +0900

    프로젝트 구성
    - spring 프로젝트 구성
    - 부트스트랩 적용
    - gitignore 추가 및 수정

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# fanservay
+# fansurvey
 아이돌 팬 유형 MBTI
---
+++ src/main/java/me/zepetto/fansurvey/FansurveyApplication.java
@@ -0,0 +1,13 @@
+package me.zepetto.fansurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FansurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FansurveyApplication.class, args);
+	}
+
+}
